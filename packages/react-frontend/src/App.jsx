615a4254b18e8edee10063836e3851e4987155ad
Jacob Marcus <jacobmarcus@Jacobs-MacBook-Air-556.local>
2025-04-01 14:46:58 -0700

    Test commit

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Testing my own text
+          djskfjlsdkfjdsafjlkdsjflksdaf
         </p>
       </div>
       <p className="read-the-docs">
