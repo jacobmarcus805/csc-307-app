3c69bab6e29bdd1dc1f36bcc779055a02c7c046c
Jacob Marcus <jacobmarcus@Jacobs-MacBook-Air-600.local>
2025-04-10 14:00:55 -0700

    Step 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -18,9 +18,17 @@ app.listen(port, () => {
 
 
 app.get("/users", (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name !== undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
   });
   
+
 const users = {
     users_list: [
       {
@@ -50,3 +58,11 @@ const users = {
       }
     ]
   };
+
+  const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  
