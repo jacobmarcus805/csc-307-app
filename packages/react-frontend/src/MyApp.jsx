be507923f9b30fa5c83ee8044eba81817f356831
Jacob Marcus <jacobmarcus@Jacobs-MacBook-Air-600.local>
2025-04-15 14:13:01 -0700

    Task 3 submit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,13 +1,25 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
+import React, {useState, useEffect} from 'react';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
+
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   function removeOneCharacter(index) {
@@ -26,6 +38,24 @@ function MyApp() {
       <Form handleSubmit={updateList} />
     </div>
   );
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
 }
 
 
